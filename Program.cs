4684bb3757a610d1dfad576c3c73cd5454d46b54
Joniclei <jonicleibernardojunior@gmail.com>
2023-05-27 22:27:04 -0300

    sd

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿/Crie um Projeto do ZERO em C#, onde esse projeto será chamado deCalculadora, dentro do arquivo Program, as seguintes opções: Calcular, Zerar,Sair, será também dentro Program que você fará a solicitação dos números e daoperação desejada. Crie uma outra classe, chamada Calculadora (em outroarquivo), ela deverá conter as 4 funções básicas e deverá retornar o resultadoda operação realizada.Seu objetivo é criar um loop de repetição, que irá realizar as operaçõesmatemáticas enquanto o usuário escolher a opção Calcular preservará oresultado da operação anterior, quando o usuário escolher zerar, irá limpar oresultado anterior e continuar a calcular, quando o usuário escolher sair, deveráencerrar o programa.*/
+
+using System;
+
+namespace Calculadora35
+{
+  public class Program
+  {
+    public static void Main (string[] args)
+    {
+      Console.WriteLine("Digite a opcao :[1]Calcular ");
+
+    }
+  }
+}
